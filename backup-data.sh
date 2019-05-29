fb2133e8e8954b633ff6d81e6913b0a86aa9fe58
Ozan Akın <ozn.akn@gmail.com>
2019-05-29 20:40:54 +0300

    Initial commit

---
+++ backup-data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -e
+set +o pipefail
+
+timestamp=$(date +%s)
+
+filename="db_$timestamp.zip"
+
+python manage.py dumpdata -e contenttypes -e auth.Permission  -e admin.LogEntry -e sessions > db.json
+
+zip -qr "db/$filename" media db.json
+
+rm -rf db.json
+
+echo "$filename"
---
+++ restore-data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+set -e
+set +o pipefail
+
+if [ ! -z "$1" ]; then
+    unzip -oq "db/$1"
+
+    python manage.py loaddata db.json
+
+    rm -rf db.json
+
+    rm "db/$1"
+
+    echo "Restore finished"
+else
+    echo "Please define a filename"
+fi
